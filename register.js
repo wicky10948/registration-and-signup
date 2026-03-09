eebd453fa364b0b313c8a9b3acf2e0fd91dec9ca
wicky10948 <smadywicky@gmail.com>
2026-03-09 10:46:33 +0300

    message

--- register.js
+++ register.js
@@ -1,12 +1,12 @@
 const API_URL = "https://iguru.co.ke/PLAYGROUND/sdg/users/register.php"; 
-document.getElementById("register").addEventListener("submit", async (e) => {
+document.getElementById("register-form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const username = document.getElementById("username").value.trim();
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  if (!username || !email || !password) {
+  if (!username ||!email || !password) {
     alert("Please fill in all fields.");
     return;
   }
